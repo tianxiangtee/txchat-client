ddf9195fa216d1e421a7072614c86b329e520e9e
Tee Tian Xiang <tianxiang.tee@kerrylogistics.com>
2021-07-07 12:22:50 +0800

    Init

---
+++ src/pages/home/Hamburger.js
@@ -0,0 +1,35 @@
+import React from "react";
+// import "./styles.css";
+import { Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { ReactComponent as Logo } from "../../logo.svg";
+import { useAuthDispatch, useAuthState } from '../../context/auth'
+
+//import "bootstrap/dist/css/bootstrap.min.css";
+
+export default function Hamburger() {
+    const authDispatch = useAuthDispatch()
+    const logout = () => {
+        authDispatch({ type: 'LOGOUT' })
+        window.location.href = '/login'
+      }
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Navbar.Brand href="#home">
+        <Logo
+          alt=""
+          width="30"
+          height="30"
+          className="d-inline-block align-top"
+        />
+        TX Chat
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="#">Coming Soon</Nav.Link>
+          <Nav.Link onClick={logout}>Logout</Nav.Link>          
+        </Nav>        
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
---
+++ src/pages/home/Home.js
@@ -0,0 +1,121 @@
+import React, { Fragment, useEffect } from 'react'
+import { Col, Row, Button } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import { gql, useSubscription } from '@apollo/client'
+
+import { useAuthDispatch, useAuthState } from '../../context/auth'
+import { useMessageDispatch } from '../../context/message'
+
+import Users from './Users'
+import Messages from './Messages'
+import Hamburger from './Hamburger'
+
+const NEW_MESSAGE = gql`
+  subscription newMessage {
+    newMessage {
+      uuid
+      from
+      to
+      content
+      createdAt
+    }
+  }
+`
+
+const NEW_REACTION = gql`
+  subscription newReaction {
+    newReaction {
+      uuid
+      content
+      message {
+        uuid
+        from
+        to
+      }
+    }
+  }
+`
+
+export default function Home({ history }) {
+  const authDispatch = useAuthDispatch()
+  const messageDispatch = useMessageDispatch()
+
+  const { user } = useAuthState()
+
+  const { data: messageData, error: messageError } = useSubscription(
+    NEW_MESSAGE
+  )
+
+  const { data: reactionData, error: reactionError } = useSubscription(
+    NEW_REACTION
+  )
+
+  useEffect(() => {
+    if (messageError) console.log(messageError)
+
+    if (messageData) {
+      const message = messageData.newMessage
+      const otherUser = user.username === message.to ? message.from : message.to
+
+      messageDispatch({
+        type: 'ADD_MESSAGE',
+        payload: {
+          username: otherUser,
+          message,
+        },
+      })
+    }
+  }, [messageError, messageData])
+
+  useEffect(() => {
+    if (reactionError) console.log(reactionError)
+
+    if (reactionData) {
+      const reaction = reactionData.newReaction
+      const otherUser =
+        user.username === reaction.message.to
+          ? reaction.message.from
+          : reaction.message.to
+
+      messageDispatch({
+        type: 'ADD_REACTION',
+        payload: {
+          username: otherUser,
+          reaction,
+        },
+      })
+    }
+  }, [reactionError, reactionData])
+
+  const logout = () => {
+    authDispatch({ type: 'LOGOUT' })
+    window.location.href = '/login'
+  }
+
+  return (
+    <Fragment>      
+      <Row className="bg-white justify-content-around mb-1">
+        {/* <Col>
+          <Link to="/login">
+            <Button variant="link">Login</Button>
+          </Link>
+        </Col>
+        <Col>
+          <Link to="/register">
+            <Button variant="link">Register</Button>
+          </Link>
+        </Col>
+        <Col>
+          <Button variant="link" onClick={logout}>
+            Logout
+          </Button>
+        </Col> */}
+        <Hamburger/>
+      </Row>
+      <Row className="bg-white">
+        <Users />
+        <Messages />
+      </Row>
+    </Fragment>
+  );
+}
